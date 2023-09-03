e217e640b845ec39476b005e88e202e9104fbfe6
Kamonrat Charoenreo <Charoenreo_k@silpakorn.edu>
2023-09-03 12:21:25 +0700

    update

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -224,7 +224,7 @@ class _HomePageState extends State<HomePage> {
                 borderRadius: BorderRadius.circular(30),
               ),
               side: BorderSide(color: Colors.black),
-              backgroundColor: Colors.green,
+              backgroundColor: Colors.lightGreen,
             ),
             child: Padding(
               padding:
